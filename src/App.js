3bcae1991bd7fecabf18b603244f6bae31fd9ded
balumahendranv <43694011+balumahendranv@users.noreply.github.com>
2024-11-13 23:15:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WELCOME TO EASYDEPLOY BALU
+          WELCOME TO EASYDEPLOY BALU CI/CD TESTING
         </p>
         <a
           className="App-link"

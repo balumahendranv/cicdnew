52c75380a07a86dbc26c0e64a0be21343faf3a2e
balumahendranv <43694011+balumahendranv@users.noreply.github.com>
2024-11-14 10:16:03 +0530

    Update2 App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WELCOME TO EASYDEPLOY TESTING
+          WELCOME TO NOTEASYDEPLOY TESTING
         </p>
         <a
           className="App-link"
